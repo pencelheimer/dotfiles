ffc78ba523064ff354ba16d3fccc7d8b0635fb61
pencelheimer <pencelheimer@proton.me>
2026-02-19 16:06:04 +0200

    feat: added throttled undervolt stub
    Stub because of problems with audio, I'm not sure if throttled is the
    issue

--- hosts/Sentinel/configuration.nix
+++ hosts/Sentinel/configuration.nix
@@ -45,6 +45,55 @@
     bitwarden-desktop
   ];
 
+  services.throttled = {
+    enable = true;
+
+    # NOTE (pencelheimer): throttled support in nixos is insufficient; the
+    # extraConig option actually rewrites the configuration entirely. I have
+    # changed only the [UNDERVOLT] section.
+    # extraConfig = let
+    #   core = "0";
+    #   cache = "0";
+    #   gpu = "0";
+    #   uncore = "0";
+    #   analogio = "0";
+    # in
+    #   # ini
+    #   ''
+    #     [GENERAL]
+    #     Enabled: True
+    #     Sysfs_Power_Path: /sys/class/power_supply/AC*/online
+    #     Autoreload: True
+    #
+    #     [BATTERY]
+    #     Update_Rate_s: 30
+    #     PL1_Tdp_W: 29
+    #     PL1_Duration_s: 28
+    #     PL2_Tdp_W: 44
+    #     PL2_Duration_S: 0.002
+    #     Trip_Temp_C: 85
+    #     cTDP: 0
+    #     Disable_BDPROCHOT: False
+    #
+    #     [AC]
+    #     Update_Rate_s: 5
+    #     PL1_Tdp_W: 44
+    #     PL1_Duration_s: 28
+    #     PL2_Tdp_W: 44
+    #     PL2_Duration_S: 0.002
+    #     Trip_Temp_C: 95
+    #     cTDP: 0
+    #     Disable_BDPROCHOT: False
+    #
+    #     [UNDERVOLT]
+    #     CORE: ${core}
+    #     CACHE: ${cache}
+    #     GPU: ${gpu}
+    #     UNCORE: ${uncore}
+    #     ANALOGIO: ${analogio}
+    #   '';
+  };
+
   services.upower.enable = true;
   services.geoclue2.enable = true;
   services.gnome.sushi.enable = true;
