1b37a2840b25060bdb09e4c146b7f320a669209c
pencelheimer <pencelheimer@proton.me>
2026-03-12 12:19:04 +0200

    feat: added gh-dash

--- homes/modules/dev/git.nix
+++ homes/modules/dev/git.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
   imports = [];
@@ -10,16 +11,19 @@
   config = lib.mkIf config.dev.git.enable {
     programs.git = {
       enable = true;
+
       settings = {
         user = {
           name = "${config.user.userName}";
           email = "${config.user.userEmail}";
           signingkey = "~/.ssh/git-pencelheimer-id.pub";
         };
+
         log = {
           showSignature = true;
           abbrevCommit = true;
         };
+
         init.defaultBranch = "main";
         fetch.prune = true;
         pull.rebase = true;
@@ -32,9 +36,44 @@
       };
     };
 
+    home.packages = with pkgs; [diffnav];
+
     programs.delta = {
       enable = true;
       enableGitIntegration = true;
+    };
+
+    programs.gh = {
+      enable = true;
+
+      hosts = {
+        "github.com".user = "${config.user.userName}";
+      };
+
+      settings = {
+        git_protocol = "ssh";
+      };
+    };
+
+    programs.gh-dash = {
+      enable = true;
+
+      settings = {
+        defaults = {
+          preview.open = true;
+          preview.width = 90;
+        };
+
+        keybindings.universl = [
+          {
+            key = "g";
+            name = "lazygit";
+            command = "cd {{.RepoPath}}; lazygit";
+          }
+        ];
+
+        pager.diff = "${lib.getExe pkgs.diffnav}";
+      };
     };
 
     programs.gpg.enable = true;
