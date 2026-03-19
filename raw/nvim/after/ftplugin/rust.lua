39735172c78e2b53c370168101ca0f20ffff7b22
pencelheimer <pencelheimer@proton.me>
2026-03-19 15:32:01 +0200

    feat(nvim): rust diagnostics hotkey

--- raw/nvim/after/ftplugin/rust.lua
+++ raw/nvim/after/ftplugin/rust.lua
@@ -1,6 +1,12 @@
 local bufnr = vim.api.nvim_get_current_buf()
 
 vim.keymap.set(
+  "n", "<leader>ld",
+  function() vim.cmd.RustLsp({ 'renderDiagnostic', 'current' }) end,
+  { silent = true, buffer = bufnr }
+)
+
+vim.keymap.set(
   "n", "<leader>la",
   function() vim.cmd.RustLsp('codeAction') end,
   { silent = true, buffer = bufnr }
